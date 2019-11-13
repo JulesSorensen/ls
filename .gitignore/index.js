658fe74b2989cec6d0484d4c3c11664f0856a603
Jules Sørensen <38363471+NekeoFaverne@users.noreply.github.com>
2019-11-13 11:51:24 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("Connecté en tant que " + client.user.tag + " !")
+    client.user.setStatus('dnd') 
+})
+
+client.login(process.env.TOKEN1)
