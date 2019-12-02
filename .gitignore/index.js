e800fa84186b73c94818458ee780fb82b389a471
Jules Sørensen <38363471+NekeoFaverne@users.noreply.github.com>
2019-12-02 21:58:57 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,10 +3,13 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log("Connecté en tant que " + client.user.tag + " !")
-    //client.user.setStatus('dnd', 'Made by KwinkyWolf')
-client.user.setActivity("with depression", {
-  type: "STREAMING",
-  url: "https://www.twitch.tv/monstercat"
-});
+
+client.user.setPresence({
+        game: {
+            name: "Overwatch",
+            name: "rien...",
+            type: 1,
+        }
+    });
 
 client.login(process.env.TOKEN1)
