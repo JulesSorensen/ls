1c29095073f9b52874f20facbf6124cfd4db84da
Jules Sørensen <38363471+NekeoFaverne@users.noreply.github.com>
2019-12-02 21:47:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,12 +4,6 @@ const client = new Discord.Client()
 client.on('ready', () => {
     console.log("Connecté en tant que " + client.user.tag + " !")
 
-    client.user.setPresence({
-        game: {
-            name: "rien...",
-            type: 1,
-        }
-    });
-})
+client.user.setStatus("dnd");
 
 client.login(process.env.TOKEN1)
