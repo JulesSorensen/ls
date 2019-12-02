df9b79413835a2d0336dc340e4178bc7b73ea418
Jules Sørensen <38363471+NekeoFaverne@users.noreply.github.com>
2019-12-02 22:00:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,8 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log("Connecté en tant que " + client.user.tag + " !")
-
+}
+          
 client.user.setPresence({
         game: {
             name: "Overwatch",
