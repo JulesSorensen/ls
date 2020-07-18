d9b575db2fd8b16a8474af5528cbd6036bd47e8b
Jules <38363471+JulesSorensen@users.noreply.github.com>
2020-07-18 19:18:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client()
 client.on('ready', () => {
     console.log("Connecté en tant que " + client.user.tag + " !")
     //client.user.setPresence({game: {type: "dnd",}});
-    client.user.setActivity('Skyforge', { type: 'dnd' });
+    client.user.setPresence({activity: {name: 'Skyforge'}, status: 'dnd'});
 });
 
 client.login(process.env.TOKEN)
